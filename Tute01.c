fe41d736b6ec1782e0bf2de22d45065ae54e9890
it21509186 <99590133+it21509186@users.noreply.github.com>
2022-02-15 00:16:27 +0530

    Update Tute01.c

--- Tute01.c
+++ Tute01.c
@@ -5,6 +5,19 @@
 #include <stdio.h>
 
 int main() {
+   int sub1,sub2;
+   float tot,avg;
+   
+   printf("Enter 1st subject mark:");
+   scanf("%d",&sub1);
+   
+   printf("Enter 2nd subject mark:");
+   scanf("%d",&sub2);
+   
+   tot=sub1+sub2;
+   avg=tot/2.0;
+   
+   printf("Your average mark is %.2f",avg);
   
   return 0;
 }
